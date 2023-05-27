0a369c3a599de8e8254f1dc991cedd809215baed
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-05-28 06:09:15 +0900

    feat: [장현민] DTO 파일 생성[DTOx]

---
+++ demo/src/main/java/com/example/demo/Account/dto/AccountDTO.java
@@ -0,0 +1,21 @@
+package com.example.demo.Account.dto;
+
+import com.example.demo.Account.entity.Account;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AccountDTO {
+    private Long accountId;
+    private String email;
+
+    public AccountDTO(Account account) {
+        this.accountId = account.getAccountId();
+        this.email = account.getEmail();
+    }
+}
---
+++ demo/src/main/java/com/example/demo/order/dto/OrderDTO.java
@@ -0,0 +1,32 @@
+package com.example.demo.order.dto;
+
+import com.example.demo.Account.dto.AccountDTO;
+import com.example.demo.product.dto.ProductDTO;
+import com.example.demo.product.entity.ProductImages;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderDTO {
+    private Long orderId;
+    private AccountDTO account;
+    private ProductDTO product;
+    private String orderStatus;
+    private String deliveryStatus;
+    private LocalDateTime createDate;
+
+    public OrderDTO(Long orderId, AccountDTO account, ProductDTO product, String orderStatus, String deliveryStatus, LocalDateTime createDate) {
+        this.orderId = orderId;
+        this.account = account;
+        this.product = product;
+        this.orderStatus = orderStatus;
+        this.deliveryStatus = deliveryStatus;
+        this.createDate = createDate;
+    }
+}
